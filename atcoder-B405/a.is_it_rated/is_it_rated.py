2b8c39b9059dfe7452311ffa3286b23dd866b9dc
unknown <wu@archquant.com>
2025-05-12 13:53:18 -0400

    code cleanup.

--- atcoder-B405/a.is_it_rated/is_it_rated.py
+++ atcoder-B405/a.is_it_rated/is_it_rated.py
@@ -1,10 +1,8 @@
 import sys
-#sys.stdin = open("input", "r")
+sys.stdin = open("input", "r")
 
-#r, x = [int(i) for i in input().split()]
+r, x = [int(i) for i in input().split()]
 
-r = 1500
-x = 2
 range = {1 : (1600, 2999), 2: (1200, 2399)}
 
 lo, hi = range[x]
--- atcoder-B405/c.sum_of_product/sum_of_product.py
+++ atcoder-B405/c.sum_of_product/sum_of_product.py
@@ -5,10 +5,11 @@ n = int(input())
 arr = [int(a) for a in input().split()]
 
 ret = 0
-# use suffix sum for efficiency
+# IM - use suffix sum for efficiency
 suffix_sum = [0] * (n+1)
 for i in range(n-1, -1, -1):
     suffix_sum[i] = suffix_sum[i+1] + arr[i]
+# suffix_sum[i+1] = sum of A(i+1) ... A(end)
 for i in range(n):
     ret += arr[i] * suffix_sum[i+1]
 print(ret) 

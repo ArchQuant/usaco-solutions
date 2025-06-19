790acb8166bb0c7b0bdfdb1377f4b5fc7e018938
unknown <wu@archquant.com>
2025-06-18 23:51:40 -0400

    add explanation.

--- simulation/shell_game/shell_game.py
+++ simulation/shell_game/shell_game.py
@@ -2,14 +2,20 @@ import sys
 sys.stdin = open("input", "r")
 
 n = int(input())
-shell_pos = list(range(3)) # IM: use this to track three scenarios at once
+# IM: use this to track three scenarios at once
+# explain: shell_pos = [empty, pebble, empty] as [0, 1, 0]
+# after shuffle, shell_pos[guess] = 1 if hit, 0 if miss
+# Now mark the shell_pos as [0, 1, 2], and counter [0, 0, 0]
+# each shell_pos[g] tracks one of the original pos of the pebble
+# counter[orignal pos] count for each pebble scenario
+shell_pos = list(range(3))
 counter = [0] * n
 for _ in range(n):
     a, b, g = [int(i) - 1 for i in input().split()]
     shell_pos[a], shell_pos[b] = shell_pos[b], shell_pos[a]
-    # IM: if originally at pos=1, only counter[g==1] will increment
-    # similarly for original pos = 2 or 3
-    # there for it tracks all three scenarios at once.
+    # IM: only counter[g==current pebble] will increment
+    # shell_pos[g] tracks the original pos of the pebble
+    # therefore it tracks all three scenarios at once.
     counter[shell_pos[g]] += 1
 
 print(max(counter))
